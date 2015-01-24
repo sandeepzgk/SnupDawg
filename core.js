c8c45bafd0ef2f2f58d320b0349ccc62230c4f46
sandeepzgk <sandeepzgk@gmail.com>
2015-01-24 08:03:33 +0000

    audio optimization for mobile playback

--- core.js
+++ core.js
@@ -75,45 +75,36 @@ function timeConverter(UNIX_timestamp){
   return time;
 }
 
-function Sound(source,volume,loop)
-{
-    this.source=source;
-    this.volume=volume;
-    this.loop=loop;
-    var son;
-    this.son=son;
-    this.finish=false;
-    this.stop=function()
-    {
-        document.body.removeChild(this.son);
+ var audiotypes={
+        "mp3": "audio/mpeg",
+        "mp4": "audio/mp4",
+        "ogg": "audio/ogg",
+        "wav": "audio/wav"
     }
-    this.start=function()
-    {
-        if(this.finish)return false;
-        this.son=document.createElement("embed");
-        this.son.setAttribute("src",this.source);
-        this.son.setAttribute("hidden","true");
-        this.son.setAttribute("volume",this.volume);
-        this.son.setAttribute("autostart","true");
-        this.son.setAttribute("loop",this.loop);
-        document.body.appendChild(this.son);
+
+    function ss_soundbits(sound){
+        var audio_element = document.createElement('audio')
+        if (audio_element.canPlayType){
+            for (var i=0; i<arguments.length; i++){
+                var source_element = document.createElement('source')
+                source_element.setAttribute('src', arguments[i])
+                if (arguments[i].match(/\.(\w+)$/i))
+                    source_element.setAttribute('type', audiotypes[RegExp.$1])
+                audio_element.appendChild(source_element)
+            }
+            audio_element.load()
+            audio_element.playclip=function(){
+                audio_element.pause()
+                audio_element.currentTime=0
+                audio_element.play()
+            }
+            return audio_element
+        }
     }
-    this.remove=function()
-    {
-        document.body.removeChild(this.son);
-        this.finish=true;
-    }
-    this.init=function(volume,loop)
-    {
-        this.finish=false;
-        this.volume=volume;
-        this.loop=loop;
-    }
-}
-var foo=new Sound("note.mp3",100,false);
+var clicksound  = ss_soundbits("note.mp3");	
 function playNotification()
 {
-	foo.start();
+	clicksound.playclip();
 
 }
 
