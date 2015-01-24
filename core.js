73fe3429fc1b0e4dc24f8bf625ff35582b8e3614
sandeepzgk <sandeepzgk@gmail.com>
2015-01-24 08:10:53 +0000

    audio fix with audio tag

--- core.js
+++ core.js
@@ -75,36 +75,10 @@ function timeConverter(UNIX_timestamp){
   return time;
 }
 
- var audiotypes={
-        "mp3": "audio/mpeg",
-        "mp4": "audio/mp4",
-        "ogg": "audio/ogg",
-        "wav": "audio/wav"
-    }
 
-    function ss_soundbits(sound){
-        var audio_element = document.createElement('audio')
-        if (audio_element.canPlayType){
-            for (var i=0; i<arguments.length; i++){
-                var source_element = document.createElement('source')
-                source_element.setAttribute('src', arguments[i])
-                if (arguments[i].match(/\.(\w+)$/i))
-                    source_element.setAttribute('type', audiotypes[RegExp.$1])
-                audio_element.appendChild(source_element)
-            }
-            audio_element.load()
-            audio_element.playclip=function(){
-                audio_element.pause()
-                audio_element.currentTime=0
-                audio_element.play()
-            }
-            return audio_element
-        }
-    }
-var clicksound  = ss_soundbits("note.mp3");	
 function playNotification()
 {
-	clicksound.playclip();
+	tone.play();
 
 }
 
